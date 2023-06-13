a1dce4b31e2c7dc8995355b31d22bdad98284ea2
VadymKrem <vadimkuzin2@gmail.com>
2023-06-13 22:22:38 +0300

    dz10 in process

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,97 +1,189 @@
 import Notiflix from 'notiflix';
-const taskURL = 'https://api.thecatapi.com/v1';
-const keyAPI = 'live_JFdKUiXjxtA7c04F8PmYcxcR6kXmZcouPK1pRumR9hppi2XhZmmqkxDfGDsjtq4Q';
 
-const refs = {
-     selectBreed : document.querySelector('.breed-select'),
-     catInfo : document.querySelector('.cat-info'),
-     spinLoader : document.querySelector('.loader'),
-     messageLoader : document.querySelector('.loader-msg'),
-     messageError: document.querySelector('.error'),
+
+// // // Напиши функцию fetchBreeds() которая делает HTTP-запрос
+// // // и возвращает промис с массивом пород - результатом запроса.
+
+function fetchBreeds(messageError) {
+    let taskURL = 'https://api.thecatapi.com/v1/breeds';
+    return fetch(taskURL)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+    .catch(error => Notiflix.Notify.failure(messageError.textContent))
+};
+
+// // // Напиши функцию fetchCatByBreed(breedId) которая ожидает идентификатор породы, делает
+// // // HTTP - запрос и возвращает промис с данными о коте - результатом запроса.
+
+function onBuildUrl(breedId) {
+    const taskUrlApi = 'https://api.thecatapi.com/v1/images/search?';
+    const keyAPI = 'live_JFdKUiXjxtA7c04F8PmYcxcR6kXmZcouPK1pRumR9hppi2XhZmmqkxDfGDsjtq4Q';
+    const searchParametres = new URLSearchParams({
+        breedId: breedId,
+        api_key: keyAPI,
+    });
+    return (taskUrlApi + searchParametres.toString());
 }
-// Напиши функцию fetchBreeds() которая делает HTTP-запрос
-// и возвращает промис с массивом пород - результатом запроса.
 
-
-function fetchBreeds(url) {
-  onLoadingInfo()
-  return fetchBreed(url)
-        .then((response) => {
-          breedSelect(response);
-          onLoadingInfo()
-          return ; 
-        })
-        .catch(() => onError());
-       };
-
-function fetchBreed(url) {
-    
-    return fetch(url)
-        .then((response) => {
-            return response.json();                        
-        })
-        .catch(() => onError());
-};
-
-// Для помилки
-function onError() {
-     Notiflix.Notify.failure('Qui timide rogat docet negare');
-};
-
-function breedSelect(breeds) {
-    const markupSelectCats =breeds.map(cat => {
-                return `<option value="${cat.id}">${cat.name}</option>`
-            }).join('');
-    refs.selectBreed.insertAdjacentHTML('afterbegin', markupSelectCats);
-    new SlimSelect({
-  select: '#single'
-})
-    return;
-};
-
-// Напиши функцию fetchCatByBreed(breedId) которая ожидает идентификатор породы, делает
-// HTTP - запрос и возвращает промис с данными о коте - результатом запроса.
-
-function fetchCatByBreed(breedId) {
-  onInformLoading2()
-    return fetchBreed(`${taskURL}/images/search?breed_ids=${breedId}&api_key=${keyAPI}`)
-        .then((response) => {
-          infoCatBreed(...response);
-          onInformLoading2()
-          return;
+function fetchCatByBreed(breedId, messageError, messageLoader, spinLoader, breedSelect) {       
+    const urlBreedCat = onBuildUrl(breedId);
+    return fetch(urlBreed)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
           
-        })
-    .catch(() => onError());
+        }
+        return response.json();
+      })
+    .catch(error => {
+        Notiflix.Notify.failure(messageError.textContent);
+        refs.spinLoader.style.display = 'none';
+        refs.messageLoader.style.display = 'none';
+        refs.selectBreed.style.display = 'block';
+        throw error;
+    })
+
+}
+
+
+export { fetchBreeds, fetchCatByBreed }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const taskURL = 'https://api.thecatapi.com/v1';
+// const keyAPI = 'live_JFdKUiXjxtA7c04F8PmYcxcR6kXmZcouPK1pRumR9hppi2XhZmmqkxDfGDsjtq4Q';
+// import SlimSelect from 'slim-select';
+
+// const refs = {
+//      selectBreed : document.querySelector('.breed-select'),
+//      catInfo : document.querySelector('.cat-info'),
+//      spinLoader : document.querySelector('.loader'),
+//      messageLoader : document.querySelector('.loader-msg'),
+//      messageError: document.querySelector('.error'),
+// }
+// // // Напиши функцию fetchBreeds() которая делает HTTP-запрос
+// // // и возвращает промис с массивом пород - результатом запроса.
+
+
+
+// function fetchBreeds(url) {
+//   onLoadingInfo()
+//   return fetchBreed(url)
+//         .then((response) => {
+//           breedSelect(response);
+//           onLoadingInfo()
+//           return ; 
+//         })
+//         .catch(() => onError());
+//        };
+
+// function fetchBreed(url) {
+    
+//     return fetch(url)
+//         .then((response) => {
+//             return response.json();                        
+//         })
+//         .catch(() => onError());
+// };
+
+// // Для помилки
+// function onError() {
+//      Notiflix.Notify.failure('Qui timide rogat docet negare');
+// };
+
+// function breedSelect(breeds) {
+//     const markupSelectCats =breeds.map(cat => {
+//                 return `<option value="${cat.id}">${cat.name}</option>`
+//             }).join('');
+//     refs.selectBreed.insertAdjacentHTML('afterbegin', markupSelectCats);
+//     new SlimSelect({
+//   select: '#single'
+// })
+//     return;
+// };
+
+
+// // // Напиши функцию fetchCatByBreed(breedId) которая ожидает идентификатор породы, делает
+// // // HTTP - запрос и возвращает промис с данными о коте - результатом запроса.
+
+// function fetchCatByBreed(breedId) {
+//   onInformLoading2()
+//     return fetchBreed(`${taskURL}/images/search?breed_ids=${breedId}&api_key=${keyAPI}`)
+//         .then((response) => {
+//           infoCatBreed(...response);
+//           onInformLoading2()
+//           return;
+          
+//         })
+//     .catch(() => onError());
   
-};
+// };
 
 
-function infoCatBreed(cat) {
-    const markupInfoCatBreed = `<img src="${cat.url}" alt='cat' width=480><div class="info-CatBreed">
-    <p class='title-cat'> ${cat.breeds[0].name}</p>
-    <p> ${cat.breeds[0].description} </p>
-    <p><span class='cat-temp'>Temperament:</span> ${cat.breeds[0].temperament}</p></div>`;
-  return refs.catInfo.innerHTML = markupInfoCatBreed;
-  refs.messageLoader.classList.toggle('is-hidden');
-  refs.spinLoader.classList.toggle('is-hidden');
-};
+// function infoCatBreed(cat) {
+//     const markupInfoCatBreed = `<img src="${cat.url}" alt='cat' width=480><div class="info-CatBreed">
+//     <p class='title-cat'> ${cat.breeds[0].name}</p>
+//     <p> ${cat.breeds[0].description} </p>
+//     <p><span class='cat-temp'>Temperament:</span> ${cat.breeds[0].temperament}</p></div>`;
+//   return refs.catInfo.innerHTML = markupInfoCatBreed;
+//   refs.messageLoader.classList.toggle('is-hidden');
+//   refs.spinLoader.classList.toggle('is-hidden');
+// };
 
-// ховає інфу про загрузку та помилку
-function onLoadingInfo() {
-  refs.messageLoader.classList.toggle('is-hidden');
-  refs.spinLoader.classList.toggle('is-hidden');
-  refs.messageError.classList.toggle('is-hidden');
-  // refs.selectBreed.classList.toggle('is-hidden');
-};
+// // ховає інфу про загрузку та помилку
+// function onLoadingInfo() {
+//   refs.messageLoader.classList.toggle('is-hidden');
+//   refs.spinLoader.classList.toggle('is-hidden');
+//   refs.messageError.classList.toggle('is-hidden');
+//   // refs.selectBreed.classList.toggle('is-hidden');
+// };
 
-// ховає селект показує лоадер та інфу про загрузку
-function onInformLoading2() {
-   refs.messageLoader.classList.toggle('is-hidden');
-  refs.spinLoader.classList.toggle('is-hidden');
-  refs.catInfo.classList.toggle('is-hidden');
-  // refs.selectBreed.classList.toggle('is-hidden');
-};
+// // ховає селект показує лоадер та інфу про загрузку
+// function onInformLoading2() {
+//    refs.messageLoader.classList.toggle('is-hidden');
+//   refs.spinLoader.classList.toggle('is-hidden');
+//   refs.catInfo.classList.toggle('is-hidden');
+//   // refs.selectBreed.classList.toggle('is-hidden');
+// };
 
 
-export { fetchBreeds, fetchCatByBreed };
+
+
+
+
+
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css'
 import 'slim-select/dist/slimselect.css';
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import "../css/loader.css";
@@ -17,13 +16,112 @@ const refs = {
      messageError: document.querySelector('.error'),
 }
 
-refs.selectBreed.addEventListener('change', onChoiceCat);
 
-function onChoiceCat(event) {
-    const choiceBreed = event.target.value;
-    return fetchCatByBreed(choiceBreed);
+refs.spinLoader.style.display = 'none';
+refs.messageLoader.style.display = 'none';
+refs.messageError.style.display = 'none';
+
+let arrayBreedsCats = [];
+
+function breedSelect(response) {
+    for(let breed in response) {
+        arrayBreedsCats.push({name: response[breed].name, id: response[breed].id})
+    }
+}
+
+// function breedSelect(breeds) {
+//     const markupSelectCats = breeds.map(cat => {
+//                 return `<option value="${cat.id}">${cat.name}</option>`
+//             }).join('');
+//     refs.selectBreed.insertAdjacentHTML('afterbegin', markupSelectCats);
+//     new SlimSelect({
+//   select: '#single'
+// })
+//     return;
+// };
+
+async function getBreeds() {
+    try {
+        const response = await fetchBreeds(refs.messageError);
+        breedSelect(response);
+        let listOfBreeds = arrayBreedsCats.map(cat => {
+            let optionCat = document.createElement('option');
+            optionCat.value = cat.id;
+            optionCat.textContent = cat.name;
+            return optionCat;
+        });
+        refs.selectBreed.append(...listOfBreeds);
+          
+    } catch (error) {
+        Notiflix.Notify.failure(refs.messageError.textContent);
+        throw error; 
+    }
+}
+
+getBreeds();
+
+
+const slimSelect = new SlimSelect({
+  select: refs.selectBreed,
+  settings: {
+    placeholderText: 'Choose the breed of the cat',
+    allowDeselect: true,
+    maxSelected: 1,
+  },
+});
+
+// function onChoiceCatBreed(event) {
+//     const choiceBreed = event.target.value;
+//     return fetchCatByBreed(choiceBreed);
        
-};
+// };
 
-fetchBreeds(`${taskURL}/breeds?${keyAPI}`);
+
+function getDataAboutBreed(data) {
+    const name = data[0].breeds[0].name;
+    const description = data[0].breeds[0].description;
+    const temperament = data[0].breeds[0].temperament;
+    const image = data[0].url;
+
+    return {'name': name, 'description': description, 'temperament': temperament, 'image': image}
+}
+
+function showBreed(promise) {
+    const elements = getDataAboutBreed(promise);
+    const {name, description, temperament, image} = dataBreed;
+
+    const markupInfoBreed = `<img src="${image}" alt="${name}" class="image"><h1>${name}</h1><p class="description">${description}</p><p class="temperament"><b class="title-temperament">Temperament: </b>${temperament}</p>`
+    refs.catInfo.innerHTML = markupInfoBreed;
+    refs.messageLoader.style.display = 'none';
+    refs.spinLoader.style.display = 'none';
+}
+
+async function onChoiceCatBreed(event) {
     
+    try {
+        const breedIndex = refs.selectBreed.options[refs.selectBreed.selectedIndex].value;
+        refs.selectBreed.style.display = 'none';
+        refs.catInfo.style.display = 'none';
+        refs.messageLoader.style.display = 'block';
+        refs.spinLoader.style.display = 'block';
+        const promise = await fetchCatByBreed(breedId);
+        showBreed(promise);
+        refs.catInfo.style.display = 'block';
+        refs.selectBreed.style.display = 'block';
+        
+    } catch (error) {
+        Notiflix.Notify.failure(refs.messageError.textContent);
+        refs.messageLoader.style.display = 'none';
+        refs.spinLoader.style.display = 'none';
+        refs.selectBreed.style.display = 'block';
+    }
+
+}
+
+
+
+
+
+refs.selectBreed.addEventListener('change', onChoiceCatBreed);
+
+// const breedId = refs.selectBreed.options[refs.selectBreed.selectedIndex].value;
